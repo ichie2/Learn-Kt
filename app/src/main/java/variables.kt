d71160ee25f8dfcd69922925e0dc86eddbcd60d9
Dammy Richie <formatable2018@gmail.com>
2022-07-26 09:31:16 +0100

    string

---
+++ app/src/main/java/variables.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val a : Int = 1
+    val b = 2
+    var c : Int
+    c = 3
+
+
+    println("a = $a, b = $b, c = $c")
+    println("x = $x, PI = $PI")
+    incrementX()
+}
+
+
+// top level variables
+
+val PI = 3.14
+var x = 0
+
+
+fun incrementX(){
+    x += 1
+}
