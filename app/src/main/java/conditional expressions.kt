93d372edcb31ddf41f34568950b551453059b61f
Dammy Richie <formatable2018@gmail.com>
2022-07-26 09:35:38 +0100

    conditional expression

--- app/src/main/java/conditional expressions.kt
+++ app/src/main/java/conditional expressions.kt
@@ -1,2 +1,21 @@
 
 
+
+
+//fun maxOf (a: Int , b: Int) : Int {
+//    if(a > b ){
+//        return a
+//    }
+//    else{
+//        return b
+//    }
+//}
+
+
+fun main() {
+    println("max of 0 and 42 is ${maxOf(0, 42)}")
+}
+
+
+// using if as an expression
+fun maxOf (a : Int, b: Int) = if (a > b)a else b
