952e4c1a36ce631a518d73dea2b688e742faea9d
Dammy Richie <formatable2018@gmail.com>
2022-07-26 09:22:47 +0100

    functions

---
+++ app/src/main/java/com/functions.kt
@@ -0,0 +1,20 @@
+package com
+
+
+
+
+//fun sum(a: Int, b:Int ): Int{
+//    return  a + b
+//}
+
+//expression body and inferred return type
+
+fun sum(a:Int, b:Int) = a +b
+
+
+
+
+fun main() {
+    print("sum of 3 and 5 is")
+    println(sum(3,5))
+}
